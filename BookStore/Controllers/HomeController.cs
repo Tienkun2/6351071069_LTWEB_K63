1509040d2a8c078ca650c266813e85d6b645ff7c
vomthang1603 <vomthang1603@gmail.com>
2024-11-01 17:28:40 +0700

    aaa

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private QLBANSACHEntities db = new QLBANSACHEntities();
+        public ActionResult Index()
+        {
+            var Sach = db.SACHes.ToList();
+            return View(Sach);
+            
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public PartialViewResult Category()
+        {
+            var category = from cd in db.CHUDEs select cd;
+            return PartialView(category);
+        }
+
+        public PartialViewResult NhaXuatBan()
+        {
+            var nhaxuatban = from cd in db.NHAXUATBANs select cd;
+            return PartialView(nhaxuatban);
+        }
+
+        public ActionResult SPtheochude(int id)
+        {
+            var sach = db.SACHes.Where(s => s.MaCD == id).ToList();
+            return View(sach);
+        }
+
+
+        public ActionResult SPtheonhaxuatban(int id)
+        {
+            var sach = db.SACHes.Where(s => s.MaNXB == id).ToList();
+            return View(sach);
+        }
+
+        public ActionResult Details (int id)
+        {
+            var sach = db.SACHes.FirstOrDefault(s => s.Masach == id);
+            return View(sach);
+        }
+    }
+}
