f2850363543a2b731bdfd1c45ed8797cae93f16d
Tienkun2 <binhtien032@gmail.com>
2024-11-08 20:45:03 +0700

    a

---
+++ BookStore/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên tài khoản.")]
+        public string TaiKhoan { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+        [DataType(DataType.Password)]
+        public string MatKhau { get; set; }
+    }
+}
---
+++ BookStore/Models/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tài khoản .")]
+        public string TaiKhoan { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email.")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+        [DataType(DataType.Password)]
+        public string MatKhau { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng xác nhận mật khẩu.")]
+        [DataType(DataType.Password)]
+        [Compare("MatKhau", ErrorMessage = "Mật khẩu không khớp.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập họ tên.")]
+        public string HoTen { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ.")]
+        public string DiaChiKH { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại.")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+        public string DienThoaiKH { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn ngày sinh.")]
+        [DataType(DataType.Date, ErrorMessage = "Ngày sinh không hợp lệ.")]
+        public DateTime Ngaysinh { get; set; }
+    }
+}
